e1902b798dd0a5bfef76d89a71b00ef0f9028264
Tanush Mahajan <tanush.mohina@gmail.com>
2023-03-14 03:54:03 +0530

    editor component init

---
+++ src/components/Editor.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Editor: React.FC = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-start">
+      <p className="text-center text-2xl text-white">Editor</p>
+    </div>
+  );
+};
